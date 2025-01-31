51bb0c16c08d539d763cf571bf28b6d6d4a3cac7
JavierPena5526 <javape5526@gmail.com>
2025-01-30 22:11:36 -0600

    Adding variables

---
+++ app.js
@@ -0,0 +1,17 @@
+var life = 100; // this is my life bar
+
+life = life - 10 ;
+
+//90
+
+life = life / 10 ;
+
+var name = "Dev Ed"; // name displayed in the game
+
+var checkout = false; // BOOLEAN
+
+var box 
+
+
+
+console.log(life);
